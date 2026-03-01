17c8d9fef0b8d641e581211fc6d51beddab5aea0
howtoee <Vijay.howto@gmail.com>
2026-03-01 12:39:35 +0530

    Updated

--- src/data/Links.jsx
+++ src/data/Links.jsx
@@ -6,37 +6,37 @@ export const socialLinks = [
   {
     id: "facebook",
     name: "Facebook",
-    url: "https://www.facebook.com/profile.php?id=61576697796195",
+    // url: "https://www.facebook.com/profile.php?id=61576697796195",
     icon: <Facebook size={18} />,
   },
   {
     id: "twitter",
     name: "Twitter (X)",
-    url: "https://x.com/Helixconfe69272",
+    // url: "https://x.com/Helixconfe69272",
     icon: <Twitter size={18} />,
   },
   {
     id: "instagram",
     name: "Instagram",
-    url: "https://www.instagram.com/helix_conferences/",
+    // url: "https://www.instagram.com/helix_conferences/",
     icon: <Instagram size={18} />,
   },
   {
     id: "youtube",
     name: "YouTube",
-    url: "https://www.youtube.com/@Helixconferences",
+    // url: "https://www.youtube.com/@Helixconferences",
     icon: <Youtube size={18} />,
   },
   {
     id: "linkedin",
     name: "LinkedIn",
-    url: "https://www.linkedin.com/company/helixconferences",
+    // url: "https://www.linkedin.com/company/helixconferences",
     icon: <Linkedin size={18} />,
   },
   {
     id: "reddit",
     name: "Reddit",
-    url: "https://www.reddit.com/user/Salty-Ad6381/",
+    // url: "https://www.reddit.com/user/Salty-Ad6381/",
     icon: <FaReddit size={18} />,
   },
 ];
